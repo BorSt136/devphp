0fdac46d67dea1accb0a84bdb32a67c0101a2b67
NameBorst <tonculte@rocketmail.com>
2020-02-05 22:14:12 +0100

    '222'

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+
+session_start();
+error_reporting(E_ALL);
+include_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/src/view');
+
+$templateEngine = new \Twig\Environment($loader);
+//activer options debug dump
+$templateEngine->enableDebug();
+$templateEngine->addExtension(new \Twig\Extension\DebugExtension());
+
+$router = new ProjetWeb\Routing\Router(include 'src/Routing/routes.php');
+
+try {
+    $controllerName = 'ProjetWeb\\Controller\\' . ucfirst($router->getController()) . 'Controller';
+    $action = $router->getAction();
+} catch (\ProjetWeb\Exception\ControllerNotFoundException|\ProjetWeb\Exception\ActionNotFoundException $exception) {
+    $controllerName = 'ProjetWeb\\Controller\\HomeController';
+    $action = 'page404';
+}
+$controller = new $controllerName($templateEngine);
+
+
+if (method_exists(get_class($controller), $action)) {
+    $controller->$action();
+} else {//superflu
+    $controller = new ProjetWeb\Controller\HomeController();
+    $controller->page404();
+}
